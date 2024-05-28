7d12f6489e64acc056615f486e45ab0d08c26e14
Filipemtb <filipera3@gmail.com>
2024-05-28 09:43:10 -0300

    consumer: expressao lambda declarada

--- interface-funcional-consumer/src/application/Main.java
+++ interface-funcional-consumer/src/application/Main.java
@@ -6,6 +6,7 @@ import model.entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Main {
 
@@ -19,7 +20,13 @@ public class Main {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        double factor = 1.1;
+
+        Consumer<Product> consumer = product -> {
+            product.setPrice(product.getPrice() * factor);
+        };
+
+        list.forEach(consumer);
         list.forEach(System.out::println);
     }
 }
