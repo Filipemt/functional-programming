c0104d5af2ec28fbc4230b7cb25cd5ca8546f688
Filipemtb <filipera3@gmail.com>
2024-05-28 09:43:50 -0300

    consumer: expressao lambda inline

--- interface-funcional-consumer/src/application/Main.java
+++ interface-funcional-consumer/src/application/Main.java
@@ -22,11 +22,7 @@ public class Main {
 
         double factor = 1.1;
 
-        Consumer<Product> consumer = product -> {
-            product.setPrice(product.getPrice() * factor);
-        };
-
-        list.forEach(consumer);
+        list.forEach(product -> product.setPrice(product.getPrice() * factor));
         list.forEach(System.out::println);
     }
 }
