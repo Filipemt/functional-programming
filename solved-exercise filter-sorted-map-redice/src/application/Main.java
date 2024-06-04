2a9608a17025ca5f314aa73c60d19e4913140b42
Filipemtb <filipera3@gmail.com>
2024-06-04 15:05:57 -0300

    criando stream para ordenar os produtos abaixo da média em forma decrescente

--- solved-exercise filter-sorted-map-redice/src/application/Main.java
+++ solved-exercise filter-sorted-map-redice/src/application/Main.java
@@ -13,10 +13,8 @@ import model.entities.Product;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -42,7 +40,18 @@ public class Main {
                     .map(p -> p.getPrice())
                     .reduce(0.0, (x,y) -> x+y) / list.size();
 
-            System.out.println("Avarage price: " + String.format("%.2f", avarage));
+            System.out.println("Avarage price: R$" + String.format("%.2f", avarage));
+
+            Comparator<String> comparator = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+            List<String> names = list.stream()
+                    .filter(p -> p.getPrice() < avarage)
+                    .map(p -> p.getName()) // Stream com o nome dos produtos filtrados
+                    .sorted(comparator.reversed())
+                    .collect(Collectors.toList());
+
+            System.out.println("Below average products: ");
+            names.forEach(System.out::println);
 
         } catch (IOException e ) {
             System.out.println("Error: " + e.getMessage());
